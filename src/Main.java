f53ffaee0503e3ddd7bc86eb0daff267956d3d32
markup54 <marzenakupka@tlen.pl>
2025-09-26 14:12:20 +0200

    losowanie liczb do arraylisty

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -6,15 +7,45 @@ public class Main {
 
         //wylosuj 10 liczb i zapisz je w tablicy
         int[] wylosowaneLiczby = wylosujLiczbyDoTablicy(10);
+        //tablica moze zawierac typy proste i zlozone
+        //nie mozna zmieniac w trakcie dzialania programu rozmiaru tablicy
         //wypisywanie tablicy
-        wypiszTablice(wylosowaneLiczby);
+        wypisz(wylosowaneLiczby);
+        //wylosuj 50 liczb bez powtorzen
+        ArrayList<Integer> wylosowaneLiczbyLista = losujLiczbyBezPowtorzen(50);
+        //ArrayLista - kolekcja
+        //musi zawierac typy zlozone czyli pisane z wielkiej litery
+        //w trakcie dzialania programu moze sie zmieniac rozmiar kolekcji
+        //wypisywanie listy
+        wypisz(wylosowaneLiczbyLista);
     }
 
-    static void wypiszTablice(int[] tablica){
+    static ArrayList<Integer> losujLiczbyBezPowtorzen(int ileLiczb){
+        Random random = new Random();
+        ArrayList<Integer> wylosowaneLiczby = new ArrayList<>();
+        for (int i = 0; i < ileLiczb; i++) {
+           int liczba = random.nextInt(100)+1;
+           while (wylosowaneLiczby.contains(liczba)){
+               liczba = random.nextInt(100)+1;
+           }
+           wylosowaneLiczby.add(liczba);
+        }
+        return wylosowaneLiczby;
+    }
+    //metoda wypisz o tej samej nazwie o różnych argumentach (typ lub liczba argumentow) -> przeciążanie metod
+
+    static void wypisz(int[] tablica){
         System.out.println("Wylosowane liczby:");
         for (int elementTablicy:tablica) {
             //typ nazwaelementu:tablica/kolekcja
             System.out.print(elementTablicy+", ");
+        }
+    }
+
+    static void wypisz(ArrayList<Integer> lista){
+        System.out.println("Lista: ");
+        for (int element:lista) {
+            System.out.print(element+", ");
         }
     }
 
